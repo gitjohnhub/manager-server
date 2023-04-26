a2f95c3400e74162ea6e4df77bb5441f533d86ba
yi <guiyc@outlook.com>
2023-04-26 14:30:35 +0800

    增加通讯录编辑功能

--- routes/helpDeskContact.js
+++ routes/helpDeskContact.js
@@ -29,5 +29,26 @@ router.post('/add', async (ctx) => {
     log4js.info(err);
   }
 });
+router.post('/update', async (ctx) => {
+  try {
+    // log4js.info(ctx.request.body);
+    const {_id,dept,dept_windows,address,note,contactNum} = ctx.request.body
+    const user = await helpDeskContact.findById(_id)
+    log4js.info(user)
+    user.dept = dept
+    user.dept_windows = dept_windows
+    user.address = address
+    user.note = note
+    user.contactNum = contactNum
+    await user.save()
+      .catch((err) => {
+        log4js.info(err);
+      });
+    ctx.body = util.success((data = user.userName));
+  } catch (err) {
+    log4js.info(err);
+  }
+});
+
 
 module.exports = router;
